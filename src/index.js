d4c3be2e55c92c7faefe3d06bbc6165968987a91
Leonardo Otero <leonardo.otero390@gmail.com>
2022-02-01 19:07:53 -0400

    first settings

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
+import './styles/reset.css';
+import './styles/style.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
