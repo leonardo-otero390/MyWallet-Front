918acb531e06e48f577298af88594659bc34c9c5
Leonardo Otero <leonardo.otero390@gmail.com>
2022-02-05 11:17:05 -0400

    chore: react envs

--- src/services/axiosBase.js
+++ src/services/axiosBase.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:4000"
+const BASE_URL = process.env.REACT_APP_API_URL === "dev"
+? "http://localhost:4000"
+: "https://my-wallet390.herokuapp.com";
 const axiosBase = axios.create({
   baseURL: BASE_URL,
 });
