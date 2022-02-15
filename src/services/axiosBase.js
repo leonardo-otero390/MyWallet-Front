d40f23f52283ed302fa1224cdec9c20a3dd398db
Liaess <yann.melo@gmail.com>
2022-02-15 16:16:06 -0300

    Preparando revisão de código

--- src/services/axiosBase.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const BASE_URL = process.env.REACT_APP_API_URL === "dev"
-? "http://localhost:4000"
-: "https://my-wallet390.herokuapp.com";
-const axiosBase = axios.create({
-  baseURL: BASE_URL,
-});
-export default axiosBase;
